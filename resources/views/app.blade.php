767f625140011fbbabd753480b3b0d6d2cf9bb92
Rafli Malik <Rafli.Malik@gmail.com>
2023-11-28 17:42:44 +0700

    Add Bootstrap 5

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link rel="shortcut icon" href="{{ asset('assets/img/favicon.png') }}" type="image/x-icon">
     @vite('resources/css/app.css')
+    @vite('resources/sass/app.scss')
     @vite('resources/js/app.jsx')
     @inertiaHead
     <meta name="csrf-token" content="{{ csrf_token() }}">
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.jsx"],
+            input: [
+                'resources/css/app.css',
+                'resources/sass/app.scss',
+                'resources/js/app.jsx',
+            ],
             refresh: true,
         }),
     ],
